fac6a81f7f629b578f5918b495fb657af1f4a387
usman479 <79809127+usman479@users.noreply.github.com>
2021-09-19 16:20:02 +0500

    qsdqw

--- Hackathon_Weekend/home.js
+++ Hackathon_Weekend/home.js
@@ -1,3 +1,4 @@
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDADH7pAKbZnqB3N9Zqc5dU-Rplk4-becI",
@@ -18,8 +19,126 @@ const firestore = firebase.firestore();
 const signOut = () => {
     firebase.auth().signOut().then(() => {
     // Sign-out successful.
-    location.href = "./index.html"
+    
   }).catch((error) => {
     // An error happened.
   });
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      
+      var uid = user.uid;
+     const displayName = document.getElementById("user");
+displayName.innerText = auth.currentUser.email;
+      // ...
+      
+    } else {
+      // User is signed out
+      // ...
+      location.href = "./index.html"
+    }
+  });
+// let live = 3
+
+
+// let colors = ["red" , "blue" , "yellow", "green", "pink", "black"];
+// let ballons = document.getElementsByClassName("balloon");
+// for(let i = 0; i < 24; i++) {
+//     let rang = colors[Math.floor(Math.random()*5+1)];
+//  ballons[i].style.backgroundColor = rang;
+//  ballons[i].setAttribute("onclick", `pop('${rang}')`)
+// }
+
+// // ballons.forEach(function (val) {
+// // console.log(val)
+// // })
+// let score = 0;
+
+
+// const pop = (b) => {
+// //     document.getElementById("lives").innerText = live;
+// //     let randomColor = document.getElementById("pop").innerText = colors[Math.floor(Math.random()*5+1)];
+// // if(b === randomColor) {
+// // score += 10;
+// // } else {
+// //     --live
+// // }
+// // document.getElementById("score").innerText = score;
+// console.log(b)
+// }
+let live = document.getElementById("lives");
+let pop = document.getElementById("pop");
+let scoreDiv = document.getElementById("score");
+let colorRange = ["purple","yellow","green","brown","blue","red"];
+let popped = 0;
+let divs = document.getElementsByClassName("balloon");
+let score = 0;
+let lives = 3;
+const colorGenerator = () => {
+    let col = colorRange[Math.floor(Math.random()*6)];
+    
+    // col.style.color = col;
+   pop.innerHTML =  `${col}`
+}
+colorGenerator();
+
+for(let i = 0; i <= 24; i++) {
+    let col = colorRange[Math.floor(Math.random()*6)];
+    divs[i].style.color = col;
+    divs[i].style.backgroundColor = col;
+    divs[i].setAttribute("onmouseover", "check(this)")
+
+}
+
+const check = (e) => {
+    if(e.style.backgroundColor === document.getElementById("pop").innerText) {
+        e.removeAttribute("onmouseover")
+        e.style.backgroundColor = "white"
+        e.innerText = "POP!"
+score += 10;
+scoreDiv.innerText = score;
+colorGenerator()
+    } else {
+        --lives; 
+        if(lives === 0) {
+            live.innerText = lives;
+alert("Game Over")
+        } else {
+            e.removeAttribute("onmouseover")
+        e.style.backgroundColor = "white"
+        e.innerText = "OOPS!"
+            live.innerText = lives;
+        colorGenerator()
+        }
+        
+    }
+}
+
+// document.addEventListener('mouseover', function(e){
+    
+//     if (e.target.className === "balloon"){
+//         pop.innerHTML = colorRange[Math.floor(Math.random()*6)];
+//                 e.target.style.backgroundColor = "#ededed";
+//                 e.target.textContent = "POP!";
+//                 popped++;
+//                 removeEvent(e);
+//                 checkAllPopped();
+//     }   
+// });
+
+// function removeEvent(e){
+//     e.target.removeEventListener('mouseover', function(){
+        
+//     })
+// };
+
+// function checkAllPopped(){
+//     if (popped === 24){
+//         console.log('all popped!');
+//         let gallery = document.querySelector('#balloon-gallery');
+//         let message = document.querySelector('#yay-no-balloons');
+//         gallery.innerHTML = '';
+//         message.style.display = 'block';
+//     }
+// };
